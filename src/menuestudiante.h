766b146ea0d9fca4d3a117ae4e1aff68ed20545d
WninaE <wninae@unjbg.edu.pe>
2024-08-05 07:01:03 -0500

    Actualizar menuestudiante.h: Agregar las declaraciones de las 4 funciones establecidas en menuestudiante.cpp

--- src/menuestudiante.h
+++ src/menuestudiante.h
@@ -15,3 +15,9 @@ struct estudiante{
 	string direccion;
 	string contrasena;
 }E[999];
+
+void menuestudiantil(string perfil, string matricula, string informacion, string menu, string opcion, estudiante E[999]);
+void perfil(string visualizar, string editar, string menu, string opcion, estudiante E[999]);
+void visualizar(string nombre, string escuela, string correo, string telefono, string departamento, string provincia, string distrito, string direccion, string menu, string opcion, estudiante E[999]);
+void editar(string correo, string telefono, string departamento, string provincia, string distrito, string direccion, string contrasena, string menu, string opcion, estudiante E[999]);
+
