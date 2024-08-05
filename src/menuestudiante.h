62de21a5dbd76c251ec14c33128a0948a5c95650
WninaE <wninae@unjbg.edu.pe>
2024-08-05 06:58:59 -0500

    Añadir menuestudiante.h: Agregar estructura o registro de estudiante

---
+++ src/menuestudiante.h
@@ -0,0 +1,17 @@
+//Encabezado del menu del estudiante
+//Nina Estuco, William Andre
+#pragma once
+#include <iostream>
+#include "menuestudiante.h"
+using namespace std;
+struct estudiante{
+	string nombres;
+	string escuela;
+	string correo_personal;
+	int telefono;
+	string departamento;
+	string provincia;
+	string distrito;
+	string direccion;
+	string contrasena;
+}E[999];
