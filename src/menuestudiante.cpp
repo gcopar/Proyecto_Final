ad411dccbddddead9e423da400ebda16f7aa7951
WninaE <wninae@unjbg.edu.pe>
2024-08-05 06:40:59 -0500

    Actualizar menuestudiante.cpp: Añadir funcion editar

--- src/menuestudiante.cpp
+++ src/menuestudiante.cpp
@@ -79,3 +79,55 @@ void visualizar(string nombre, string escuela, string correo, string telefono, s
 		}
 	}while(n!=0);
 }
+
+void editar(string correo, string telefono, string departamento, string provincia, string distrito, string direccion, string contrasena, string menu, string opcion, estudiante& E[999]){
+	int n;
+	do{
+		cout<<"EDITAR"<<endl;
+		cout<<correo<<endl;
+		cout<<telefono<<endl;
+		cout<<departamento<<endl;
+		cout<<provincia<<endl;
+		cout<<distrito<<endl;
+		cout<<direccion<<endl;
+		cout<<contrasena<<endl;
+		cout<<menu<<endl; 
+		cout<<opcion; cin>>n;
+		switch(n){
+			case 1:
+				cout<<"Ingrese nuevo correo personal: "; cin>>E[i].correo_personal; cout<<endl;
+				cout<<"Cambios guardados"<<endl;
+				break;
+			case 2:
+				cout<<"Ingrese nuevo # telefono: "; cin>>E[i].telefono; cout<<endl;
+				cout<<"Cambios guardados"<<endl;
+				break;
+			case 3:
+				cout<<"Ingrese nuevo departamento: "; cin>>E[i].departamento; cout<<endl;
+				cout<<"Cambios guardados"<<endl;
+				break;
+			case 4:
+				cout<<"Ingrese nueva provincia: "; cin>>E[i].provincia; cout<<endl;
+				cout<<"Cambios guardados"<<endl;
+				break;
+			case 5:
+				cout<<"Ingrese nuevo distrito: "; cin>>E[i].distrito; cout<<endl;
+				cout<<"Cambios guardados"<<endl;
+				break;
+			case 6:
+				cout<<"Ingrese nueva direccion: "; cin>>E[i].direccion; cout<<endl;
+				cout<<"Cambios guardados"<<endl;
+				break;
+			case 7:
+				cout<<"Ingrese nueva contrasena: "; cin>>E[i].contrasena; cout<<endl;
+				cout<<"Cambios guardados"<<endl;
+				break;
+			case 0:
+				perfil("1. Visualizar", "2. Editar", "3. Menu anterior", "Ingrese una opcion: ", E[i]);
+				break;
+			default:
+    			cout<<"Opcion no valida, intentelo de nuevo.";
+    			break;
+		}
+	}while(n!=0);
+}
