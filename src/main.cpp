8f409992ef2be98533f8975af0a769445bdede5b
gcopar <gcopar@unjbg.edu.pe>
2024-08-09 11:35:43 -0500

    Se agrego una funcion login en main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include "menu.h"
+#include "usuarios.h"
+#include "curso.h"
+#include "nota.h"
+#include "asistencia.h"
+
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int main(){
+    menu_login();
+    return 0;
+}
